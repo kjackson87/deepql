fd0071f74cdfa23b575f468c99efbe043c09f0a8
Kyle Jackson <kjacks@microsoft.com>
2021-12-21 06:35:54 -0800

    Add in meter class for capturing metrics

--- utils.py
+++ utils.py
@@ -10,3 +10,20 @@ def plot_rewards(rewards):
 def moving_average(data, window):
   start_window = np.full(window, np.nan)
   return np.append(start_window, np.convolve(data, np.ones(window), 'valid') / window)
+
+class Meter():
+  def __init__(self, name):
+    self.name = name
+    self.reset()
+
+  def reset(self):
+    self.values = []
+
+  def update(self, val):
+    self.values.append(val)
+
+  def plot(self, ax=None):
+    if ax == None:
+      fig, ax = plt.subplots()
+    ax.plot(self.values)
+    ax.set_title(self.name)
